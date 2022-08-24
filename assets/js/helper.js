ceeb9b5453db3279d4d50fc606e65266460fd08d
jolayemi_local <ajolayemi82@gmail.com>
2022-08-24 23:41:40 +0200

    Defined buildDateRange function

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,3 +1,7 @@
+// Holds the value of the first year to include in filter
+const firstYear = 1980;
+
+// selected parts of the webpage
 const filterGenreCls = document.querySelector('.movies .genre');
 
 // Returns the current genre selection from the dropdown menu
@@ -15,4 +19,37 @@ const addGenres = (genresToAdd) => {
         filterGenreCls.appendChild(optionEl)
     }
 
+}
+
+// Builds an obj where keys are date range and values is an Array of
+// dates in the specified range.
+
+const buildDateRange = () => {
+    const currentYear = new Date().getFullYear();
+    const yearRange = currentYear - firstYear;
+    const yearArray = Array(yearRange).fill(0).map((_, i) => i + firstYear);
+
+    const step = 10
+    let start = 0;
+    let stop = yearArray.length;
+
+    let goOn = true;
+    let yearObj = {};
+    for (let end=10; goOn; end+=10) {
+        if (end < stop) {
+            let segment = yearArray.slice(start, end);
+            start += 10;
+            let key = `${segment[0]} - ${segment[step - 1]}`;
+            yearObj[key] = segment;
+        }
+        
+        else {
+            let difference = yearRange - start;
+            let segment = yearArray.slice(start);
+            let key = `${segment[0]} - ${segment[difference - 1]}`;
+            yearObj[key] = segment;
+            goOn = false;
+        }
+    }
+return yearObj;
 }
